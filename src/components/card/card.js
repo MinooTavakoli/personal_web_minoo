45e12f15975c11e67347d9e001d61acabf3be211
Minoo Tavakoli <m.tavakoli@sibitec.com>
2023-03-18 12:37:21 +0330

    add animate to card

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,8 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { motion, useIsPresent, useScroll, useSpring } from "framer-motion";
 import { Loading, Disabled } from "../tools";
 import "./card.css";
-
 
 function Card({
   children,
@@ -18,6 +18,32 @@ function Card({
   titleStyle = {},
   ...rest
 }) {
+  // const leftOpacity = {
+  //   hidden: {
+  //     opacity: 0,
+  //     x: "25px",
+  //     translateX: "-25px",
+  //   },
+  //   visible: {
+  //     opacity: 1,
+  //     x: 0,
+  //     // transition: { type: "spring", stiffness: 100 },
+  //     translateX: "0",
+  //   },
+  //   close: {
+  //     opacity: 0,
+  //     x: "-25px",
+  //     translateX: "25px",
+  //   },
+  // };
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
+  const isPresent = useIsPresent();
+
   return (
     <div className={`card${tabs ? " card-tabs " : " "}${className}`} {...rest}>
       {loading && <Loading />}
@@ -36,7 +62,32 @@ function Card({
       )}
       {children && (
         <div className="card-content" style={contentStyle}>
-          {children}
+          <motion.div className="progress" style={{ scaleX }} />
+          <motion.div
+            initial={{ scaleX: 0 }}
+            animate={{
+              scaleX: 1,
+              transition: { duration: 0.5, ease: "circOut" },
+            }}
+            exit={{ scaleX: 1, transition: { duration: 0.5, ease: "circIn" } }}
+            style={{ originX: isPresent ? 0 : 1 }}
+            className="privacy-screen"
+          >
+            {children}
+          </motion.div>
+          {/* <AnimatePresence>
+            <motion.div
+              key={"1"}
+              variants={leftOpacity}
+              initial="hidden"
+              animate="visible"
+              exit="close"
+              className={`h-full ${className}`}
+              transition={{ type: "spring", stiffness: 150 }}
+            >
+              {children}
+            </motion.div>
+          </AnimatePresence> */}
         </div>
       )}
       {/* {cardContent && <div>{cardContent}</div>} */}
